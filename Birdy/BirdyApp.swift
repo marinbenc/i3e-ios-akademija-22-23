d014b9c2b73be10433f7abf018f7cbb7f7e7e24b
marinbenc <marinbenc@gmail.com>
2022-12-06 17:18:55 +0100

    Initial Commit

---
+++ Birdy/BirdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdyApp.swift
+//  Birdy
+//
+//  Created by Marin Bencevic on 06.12.2022..
+//
+
+import SwiftUI
+
+@main
+struct BirdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
